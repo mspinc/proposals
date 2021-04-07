874f4eb15dcabfd2e5f6c73e0e74807b3e06a368
Banff International Research Station <77022197+birs-math@users.noreply.github.com>
2021-04-07 12:21:57 -0600

    Added FactoryBot, Faker, created first rspec test [Finishes #177564927]

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -61,4 +61,8 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
+
+  # Incude supporting ruby files with custom matchers and macros, etc,
+  # in spec/support/ and its subdirectories.
+  Dir[Rails.root.join("spec/support/**/*.rb")].each { |f| require f }
 end
