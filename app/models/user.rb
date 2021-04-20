e392483a61eba526baadab9082ac286056dbab91
Hassan Mahmood <devhassan.mahmood@gmail.com>
2021-04-20 16:08:39 +0500

    Add user confirmable

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,10 +2,9 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable, :confirmable
 
   has_many :user_roles, dependent: :destroy
   has_many :roles, through: :user_roles
-  has_one :staff
   has_one :organizer
 end
