5281b8a14cacc3eaba7cd368de1ad87676c62286
Rizwan Ali <devali.rizwan@gmail.com>
2021-05-18 20:08:22 +0500

    Feature Tests

--- spec/features/locations_show_spec.rb
+++ spec/features/locations_show_spec.rb
@@ -1,5 +1,35 @@
 require 'rails_helper'
 
 RSpec.feature "Locations show", type: :feature do
-  scenario "Locations#show feature tests..."
+  #@scenario "Locations#show feature tests..."
+
+before do
+    @location = create(:location)
+    visit location_path(@location)
+  end
+
+  scenario "there is a Name field containing the name" do
+    expect(find_by_id('location_name').text).to eq(@location.name)
+  end
+
+  scenario "there is a Code field containing the code" do
+    expect(find_by_id('location_code').text).to eq(@location.code)
+  end
+
+  scenario "there is a City field containing the city" do
+    expect(find_by_id('location_city').text).to eq(@location.city)
+  end
+
+  scenario "there is a Country field containing the country" do
+    expect(find_by_id('location_country').text).to eq(@location.country)
+  end
+
+  scenario "click on edit" do
+    expect(page).to have_link(href: edit_location_path(@location))
+  end
+
+  scenario "click on back" do
+    expect(page).to have_link(href: locations_path)
+  end
+
 end
---
+++ spec/features/proposal_forms_index_spec.rb
@@ -0,0 +1,48 @@
+require 'rails_helper'
+
+RSpec.feature "Proposal_Form index", type: :feature do
+  before do
+    3.times { create(:proposal_form) }
+    visit proposal_forms_path
+  end
+
+  scenario "list all existing proposal Forms" do
+    ProposalForm.all.each do |proposal_form|
+      expect(page).to have_text(proposal_form.proposal_type.name)
+      proposal_form.proposal_type.locations.each do |loc|
+        expect(page).to have_text(loc.name)
+      end
+      
+      expect(page).to have_text(proposal_form.status)
+      expect(page).to have_text(proposal_form.created_at.to_date)
+      expect(page).to have_text(proposal_form.created_by.email)
+      expect(page).to have_text(proposal_form.updated_by.email)
+    end
+  end
+
+  scenario "there is a link to proposal_form_#show" do
+    ProposalForm.all.each do |proposal_form|
+      expect(page).to have_link(href: proposal_form_path(proposal_form))
+    end
+  end
+
+  scenario "there is a link to ProposalForm#destroy" do
+    ProposalForm.all.each do |proposal_form|
+      del_link = "a[href='#{proposal_form_path(proposal_form)}'][data-method='delete']"
+      expect(page).to have_selector(del_link)
+    end
+  end
+
+
+  scenario "there is a link to proposal_form_#edit" do
+    ProposalForm.all.each do |proposal_form|
+      if proposal_form.status == 'draft'
+        expect(page).to have_link(href: edit_proposal_form_path(proposal_form))
+      end
+    end
+  end  
+
+
+  
+
+end
