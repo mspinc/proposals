d67550e096ecf5f48b58d65bffa4bc271e1907b3
Hassan Mahmood <devhassan.mahmood@gmail.com>
2021-05-26 19:16:19 +0500

    Fix factory

--- spec/factories/invites.rb
+++ spec/factories/invites.rb
@@ -1,13 +1,12 @@
 require 'factory_bot_rails'
-require 'faker'
 
 FactoryBot.define do
   factory :invite do |f|
     f.firstname
     f.lastname
     f.email
-    f.invited_as
-
+    
+    invited_as { %w[participant coorganizer]}
     status { %w[pending completed].sample }
     response { %w[yes maybe not].sample }
     association :proposal, factory: :proposal, strategy: :create
