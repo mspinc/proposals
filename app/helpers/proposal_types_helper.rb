d561908eac58bb26c4b9e42dc4cbca4bc8e12039
Banff International Research Station <77022197+birs-math@users.noreply.github.com>
2021-06-07 17:56:21 -0600

    Add locations to proposal_type list

--- app/helpers/proposal_types_helper.rb
+++ app/helpers/proposal_types_helper.rb
@@ -1,2 +1,7 @@
 module ProposalTypesHelper
+  def list_proposal_locations(proposal_type)
+    return '' if proposal_type.locations.empty?
+
+    proposal_type.locations.map(&:name).join("<br>\n").html_safe
+  end
 end
