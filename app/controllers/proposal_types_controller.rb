2beb155adf7d2039f82f755634efc9a133d8c9a3
Hassan Mahmood <devhassan.mahmood@gmail.com>
2021-06-09 16:37:32 +0500

    fixes

--- app/controllers/proposal_types_controller.rb
+++ app/controllers/proposal_types_controller.rb
@@ -50,7 +50,7 @@ class ProposalTypesController < ApplicationController
   private
 
   def proposal_type_params
-   params.require(:proposal_type).permit(:name, :co_organizer, :participant, location_ids: [])
+   params.require(:proposal_type).permit(:name, :year, :co_organizer, :participant, location_ids: [])
   end
 
   def set_proposal_type
