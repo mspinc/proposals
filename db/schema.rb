849bea66476ae075987f1419ff83322159d86c77
Rizwan Ali <devali.rizwan@gmail.com>
2021-06-09 18:17:15 +0500

    Make subject id optional in ams subject

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_08_191917) do
+ActiveRecord::Schema.define(version: 2021_06_09_120256) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
@@ -19,7 +19,7 @@ ActiveRecord::Schema.define(version: 2021_06_08_191917) do
   create_table "ams_subjects", force: :cascade do |t|
     t.string "code"
     t.string "title"
-    t.bigint "subject_id", null: false
+    t.bigint "subject_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["subject_id"], name: "index_ams_subjects_on_subject_id"
