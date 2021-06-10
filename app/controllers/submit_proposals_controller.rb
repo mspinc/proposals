c50a70e0774b37094769684cf4fa817842fdfbad
Rizwan Ali <devali.rizwan@gmail.com>
2021-06-10 19:36:37 +0500

    Add validation for subjects

--- app/controllers/submit_proposals_controller.rb
+++ app/controllers/submit_proposals_controller.rb
@@ -6,6 +6,8 @@ class SubmitProposalsController < ApplicationController
   def create
     @proposal = Proposal.find params[:proposal]
     @proposal.update(proposal_params)
+    update_ams_subject_code
+    
     @publish = true
     if SubmitProposalService.new(@proposal, params).save_answers
       redirect_to thanks_submit_proposals_path, notice: 'Your proposal has been submitted'
@@ -19,6 +21,17 @@ class SubmitProposalsController < ApplicationController
   private
 
   def proposal_params
-    params.permit(:title, :year, :subject_id, ams_subject_ids: [])
+    params.permit(:title, :year, :subject_id).merge(ams_subject_ids: proposal_ams_subjects)
   end
+
+  def proposal_ams_subjects
+    @param = params[:ams_subjects]
+    [@param[:code1], @param[:code2]]
+  end
+
+  def update_ams_subject_code
+    @proposal.ams_subjects.where(id: @param[:code1])&.update(code: 'code1')
+    @proposal.ams_subjects.where(id: @param[:code2])&.update(code: 'code2')
+  end
+
 end
