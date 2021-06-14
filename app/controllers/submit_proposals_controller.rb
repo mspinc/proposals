5441d2af47187f1e8f0050ef6d400b69e8f7c739
Rizwan Ali <devali.rizwan@gmail.com>
2021-06-14 15:58:38 +0500

    UI changes

--- app/controllers/submit_proposals_controller.rb
+++ app/controllers/submit_proposals_controller.rb
@@ -7,12 +7,15 @@ class SubmitProposalsController < ApplicationController
     @proposal = Proposal.find params[:proposal]
     @proposal.update(proposal_params)
     update_ams_subject_code
+    
+     @publish = true if params[:commit] == 'Submit Proposal'
 
-    @publish = true
     if SubmitProposalService.new(@proposal, params).save_answers
       redirect_to thanks_submit_proposals_path, notice: 'Your proposal has been submitted'
     else
       flash[:alert] = 'There are some errors, please see form.' if params[:commit] == 'Submit Proposal'
+      flash[:notice] = 'Draft saved.' if params[:commit] == 'Save as draft'
+      
       redirect_to edit_proposal_path(@proposal, publish: @publish)
     end
   end
