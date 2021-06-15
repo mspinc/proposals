83fe2fb6d0d75574ae137d05b27d87bc3524db55
Banff International Research Station <brentk@birs.ca>
2021-06-15 11:10:23 -0600

    moved validation to the model

--- app/models/invite.rb
+++ app/models/invite.rb
@@ -1,5 +1,6 @@
 class Invite < ApplicationRecord
   validates :firstname, :lastname, :email, :invited_as, :deadline_date, presence: true
+  validate :proposal_title
   enum status: { pending: 0, completed: 1 }
   enum response: { yes: 0, maybe: 1, no: 2 }
 
@@ -10,5 +11,12 @@ class Invite < ApplicationRecord
 
   def generate_code
     self.code = SecureRandom.urlsafe_base64(37) if self.code.blank?
+  end
+
+  def proposal_title
+    return if self.proposal.nil?
+    if proposal.title.blank?
+      errors.add('Proposal Title:', 'Please add a title to the proposal.')
+    end
   end
 end
