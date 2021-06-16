b30defedd4b7312654980bcdf3176253464b273d
Hassan Mahmood <devhassan.mahmood@gmail.com>
2021-06-16 19:30:15 +0500

    Add validation for preferred dates

--- app/models/validation.rb
+++ app/models/validation.rb
@@ -2,5 +2,6 @@ class Validation < ApplicationRecord
   belongs_to :proposal_field
   enum validation_type: { mandatory: 0, 'less than (integer matcher)': 1, 'less than (float matcher)': 2,
                           'greater than (integer matcher)': 3, 'greater than (float matcher)': 4,
-                          'equal (string matcher)': 5, 'equal (integer matcher)': 6, 'equal (float matcher)': 7 }
+                          'equal (string matcher)': 5, 'equal (integer matcher)': 6, 'equal (float matcher)': 7,
+                          '5-day workshop preferred/Impossible dates': 8 }
 end
