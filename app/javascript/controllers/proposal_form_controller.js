ff9dbeb4c88025933a6274e6063066ebfc5cd8b8
Hassan Mahmood <devhassan.mahmood@gmail.com>
2021-06-18 19:17:16 +0500

    Clear interval

--- app/javascript/controllers/proposal_form_controller.js
+++ app/javascript/controllers/proposal_form_controller.js
@@ -7,10 +7,7 @@ export default class extends Controller {
 
   connect () {
     let url = window.location.href.split('/').slice(-3)
-    if(url.includes('proposals') && url.includes('edit')) {
-      let id = url[1]
-      this.autoSaveProposal(id);
-    }
+    this.autoSaveProposal(url);
   }
 
   disableOtherInvites () {
@@ -115,12 +112,20 @@ export default class extends Controller {
     }
   }
 
-  autoSaveProposal (id) {
-    setInterval(function() {
-      $.post(`/submit_proposals?proposal=${id}`,
-        $('form#submit_proposal').serialize(), function(data) {
-        console.log('auto saving...')
-      })  
-    }, 10000);
+  autoSaveProposal (url) {
+    var interval;
+    if(url.includes('proposals') && url.includes('edit')) {
+      let id = url[1]
+      interval =  setInterval(function() {
+        $.post(`/submit_proposals?proposal=${id}`,
+          $('form#submit_proposal').serialize(), function(data) {
+          console.log('auto saving...')
+        })  
+      }, 10000);
+      localStorage.setItem('interval', interval)
+    } else {
+      clearInterval(localStorage.getItem('interval'))
+      localStorage.removeItem("interval");
+    }
   }
 }
