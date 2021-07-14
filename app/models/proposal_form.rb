ef97de81ea32fdc59b1d9db2409402e5e6d310d4
Hassan Mahmood <devhassan.mahmood@gmail.com>
2021-07-14 17:15:58 +0500

    Fixed extra spaces

--- app/models/proposal_form.rb
+++ app/models/proposal_form.rb
@@ -5,7 +5,7 @@ class ProposalForm < ApplicationRecord
   belongs_to :updated_by, class_name: 'User'
   has_many(:proposal_fields,
            -> { order(position: :asc) },
-           inverse_of: :proposal_form,  dependent: :destroy)
+           inverse_of: :proposal_form, dependent: :destroy)
   enum status: { draft: 0, active: 1, inactive: 2 }
   
   scope :active_form, -> (id) { where(proposal_type_id: id, status: :active)&.last }
