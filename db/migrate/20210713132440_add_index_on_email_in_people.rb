00019cd485f3fe6de378e357481280ef4d868ad2
Banff International Research Station <brentk@birs.ca>
2021-07-15 13:44:16 -0600

    * run rake task in migration to remove duplicate Person records * don't nuke records in production * don't try to create duplicate Person records

--- db/migrate/20210713132440_add_index_on_email_in_people.rb
+++ db/migrate/20210713132440_add_index_on_email_in_people.rb
@@ -1,5 +1,6 @@
 class AddIndexOnEmailInPeople < ActiveRecord::Migration[6.1]
   def change
+    Rake::Task['birs:remove_duplicate_person_records'].invoke
     add_index :people, :email, unique: true
   end
 end
