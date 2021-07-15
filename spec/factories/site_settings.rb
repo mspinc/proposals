237f9a181e828c04af1ff729580661527c68e9ea
Rizwan Ali <devali.rizwan@gmail.com>
2021-07-15 19:30:15 +0500

    fix rspec

---
+++ spec/factories/site_settings.rb
@@ -0,0 +1,5 @@
+FactoryBot.define do
+  factory :site_setting do
+    guideline {Faker::Lorem.paragraph}
+  end
+end
--- spec/requests/pages_spec.rb
+++ spec/requests/pages_spec.rb
@@ -3,6 +3,7 @@ require 'rails_helper'
 RSpec.describe "/pages", type: :request do
   describe "GET guidelines" do
     before do
+      create(:site_setting)
       get guidelines_url
     end
     it { expect(response).to have_http_status(:ok) }
