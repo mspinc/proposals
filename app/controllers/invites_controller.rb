e238461f035144a59d325ecec79ead8830da928b
Rizwan Ali <devali.rizwan@gmail.com>
2021-07-28 18:24:32 +0500

    fix codacy issue

--- app/controllers/invites_controller.rb
+++ app/controllers/invites_controller.rb
@@ -5,7 +5,7 @@ class InvitesController < ApplicationController
   before_action :set_invite_proposal, only: %i[show]
 
   def show
-    redirect_to root_path, alert: "Invite code is invalid" and return if @invite.nil? 
+    redirect_to root_path, alert: "Invite code is invalid" and return if @invite.nil?
     redirect_to root_path and return if @invite.confirmed?
     redirect_to expired_path and return if @invite.cancelled?
 
