438f7d11a58de5ed73a21b5bfd6d1f4d9ee0bec1
Rizwan Ali <devali.rizwan@gmail.com>
2021-08-02 19:16:42 +0500

    Added tests for mailers

--- spec/mailers/feedback_mailer_spec.rb
+++ spec/mailers/feedback_mailer_spec.rb
@@ -1,5 +1,12 @@
 require "rails_helper"
 
 RSpec.describe FeedbackMailer, type: :mailer do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:feedback) { create(:feedback) }
+  let(:mail) { FeedbackMailer.with(feedback: feedback).new_feedback_email.deliver_now }
+  it 'renders the subject' do
+    expect(mail.subject).to eq('You got a new feedback!')
+  end
+  it 'renders the receiver email' do
+    expect(mail.to).to eq(["birs@birs.ca"])
+  end
 end
--- spec/mailers/invite_spec.rb
+++ spec/mailers/invite_spec.rb
@@ -1,5 +1,51 @@
 require "rails_helper"
 
 RSpec.describe InviteMailer, type: :mailer do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "invite email" do
+    let(:invite) { create(:invite) }
+    let(:mail) { InviteMailer.with(invite: invite).invite_email.deliver_now }
+    let(:s) { 'BIRS Proposal: Invite for ' + invite.invited_as? }
+    it 'renders the subject' do
+      expect(mail.subject).to eq(s)
+    end
+    it 'renders the receiver email' do
+      expect(mail.to).to eq([invite.person.email])
+    end
+  end
+
+  describe "invite acceptance" do
+    let(:invite) { create(:invite, invited_as: 'Co Organizer') }
+    let(:co_organizers) { invite.proposal.list_of_co_organizers.remove(invite.person&.fullname) }
+    let(:mail) { InviteMailer.with(invite: invite, co_organizers: co_organizers).invite_acceptance.deliver_now }
+    it 'renders the subject' do
+      expect(mail.subject).to eq('BIRS Proposal: RSVP Confirmation')
+    end
+    it 'renders the receiver email' do
+      expect(mail.to).to eq([invite.person.email])
+    end
+  end
+
+  describe "invite decline" do
+    let(:invite) { create(:invite) }
+    let(:mail) { InviteMailer.with(invite: invite).invite_decline.deliver_now }
+    it 'renders the subject' do
+      expect(mail.subject).to eq('Invite Declined')
+    end
+    it 'renders the receiver email' do
+      expect(mail.to).to eq([invite.person.email])
+    end
+  end
+
+  describe "invite reminder" do
+    let(:invite) { create(:invite, invited_as: 'Co Organizer') }
+    let(:co_organizers) { invite.proposal.list_of_co_organizers.remove(invite.person&.fullname) }
+    let(:mail) { InviteMailer.with(invite: invite, co_organizers: co_organizers).invite_reminder.deliver_now }
+    let(:s) { 'Please Respond – BIRS Proposal: Invite for ' + invite.invited_as? }
+    it 'renders the subject' do
+      expect(mail.subject).to eq(s)
+    end
+    it 'renders the receiver email' do
+      expect(mail.to).to eq([invite.person.email])
+    end
+  end
 end
