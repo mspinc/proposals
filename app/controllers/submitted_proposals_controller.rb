f32bb0c21ed4b696388035bd2ae28b4fac016bb2
Hassan Mahmood <devhassan.mahmood@gmail.com>
2021-08-09 20:52:39 +0500

    Submitted proposals sent on editFlow

--- app/controllers/submitted_proposals_controller.rb
+++ app/controllers/submitted_proposals_controller.rb
@@ -12,6 +12,18 @@ class SubmittedProposalsController < ApplicationController
     send_data @proposals.to_csv, filename: "submitted_proposals.csv"
   end
 
+  def edit_flow
+    params[:ids]&.split(',')&.each do |id|
+      @proposal = Proposal.find_by(id: id.to_i)
+      curl_request
+    end
+
+    respond_to do |format|
+      format.js { render js: "window.location='/submitted_proposals'" }
+      format.html { redirect_to submitted_proposals_path, notice: 'Successfully sent proposal(s) to EditFlow!' }
+    end
+  end
+
   private
 
   # rubocop:disable Metrics/AbcSize
@@ -28,6 +40,82 @@ class SubmittedProposalsController < ApplicationController
       @proposals = query.find(params)
     else
       @proposals = Proposal.active_proposals
+    end
+  end
+
+  def latex_temp_file
+    "propfile-#{current_user.id}-#{@proposal.id}.tex"
+  end
+
+  def file_path
+    ProposalPdfService.new(@proposal.id, latex_temp_file, 'all').pdf
+    @year = @proposal&.year || Date.current.year.to_i + 2
+    fh = File.open("#{Rails.root}/tmp/#{latex_temp_file}")
+    @latex_input = fh.read
+
+    pdf = render_to_string layout: "application", inline: "#{@latex_input}", formats: [:pdf]
+
+    @pdf_path = "#{Rails.root}/tmp/submit-#{DateTime.now.to_i}.pdf"
+    upload = File.open(@pdf_path, 'w:binary') do |file|
+      file.write(pdf)
+    end
+  end
+
+  def curl_request
+    file_path
+
+    country_code = Country.find_country_by_name(@proposal.lead_organizer.country)
+    co_organizers = @proposal.invites.where(invited_as: 'Co Organizer')
+    country_code_organizers = Country.find_country_by_name(co_organizers.first.person.country)
+    query = <<END_STRING
+            mutation {
+              article: submitArticle(data: {
+                authors: [{
+                  email: "#{@proposal.lead_organizer.email}"
+                  givenName: "#{@proposal.lead_organizer.firstname}"
+                  familyName: "#{@proposal.lead_organizer.lastname}"
+                  nameInOriginalScript: "日暮 ひぐらし かごめ"
+                  institution: "#{@proposal.lead_organizer.affiliation}"
+                  countryCode: "#{country_code.alpha2}"
+                }, {
+                  email: "#{co_organizers.first.email}"
+                  givenName: "#{co_organizers.first.firstname}"
+                  familyName: "#{co_organizers.first.lastname}"
+                  institution: "#{co_organizers.first.person.affiliation}"
+                  countryCode: "#{country_code_organizers.alpha2}"
+                  mrAuthorID: 12345
+                }]
+                correspAuthorEmail: "#{@proposal.lead_organizer.email}"
+                title: "#{@proposal.title}"
+                sectionAbbrev: "#{@proposal.subject.code}"
+                primarySubjects: {
+                  scheme: "MSC2020"
+                  codes: ["00-01"]
+                }
+                secondarySubjects: {
+                  scheme: "MSC2020"
+                  codes: ["00B05", "00A07"]
+                }
+                abstract: "#{@proposal.title}"
+                files: [{
+                  role: "main"
+                  key: "fileMain"
+                }]
+              }) {
+                identifier
+              }
+            }
+END_STRING
+
+    response = RestClient.post 'https://ef-demo.msp.org/efdemo_y/api/test/birs/',
+      {:query => query, :fileMain => File.open(@pdf_path)},
+      {:x_editflow_api_token => 'ovmpaE-cR3Kt3qUUY7KAhO20X-XIxslovfPwNY-MUfM='}
+    puts response
+
+    if response.body.include?("errors")
+      flash[:alert] = "Request cannot be sent!"
+    else
+      flash[:notice] = "Request sent successfully!"
     end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -18,6 +18,7 @@ Rails.application.routes.draw do
   resources :submitted_proposals do
     collection do
       get :download_csv
+      post :edit_flow
     end
   end
 
