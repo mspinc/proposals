72cd04d919953425735ef6c3384a99e146bde474
Hassan Mahmood <devhassan.mahmood@gmail.com>
2021-08-11 17:54:49 +0500

    Fixed codacy error

--- app/models/person.rb
+++ app/models/person.rb
@@ -35,6 +35,7 @@ class Person < ApplicationRecord
   # rubocop:disable Metrics/AbcSize
   def common_fields
     return if skip_person_validation
+
     errors.add('Main Affiliation/Institution', "can't be blank") if affiliation.blank?
     errors.add('Academic Status', "can't be blank") if academic_status.blank?
     errors.add('Year of', "PhD can't be blank") if first_phd_year.blank?
