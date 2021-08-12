55603391b2382469fb55eeeac6a9029fbb6875f2
Hassan Mahmood <devhassan.mahmood@gmail.com>
2021-08-12 18:58:57 +0500

    Updated migration

--- db/migrate/20210717065007_update_subject_table.rb
+++ db/migrate/20210717065007_update_subject_table.rb
@@ -2,7 +2,7 @@ class UpdateSubjectTable < ActiveRecord::Migration[6.1]
   def change
     Subject.find_by(code: "ARNT")&.update(code: "ARGEO", title: "Arithmetic Geometry")
     subject_category = SubjectCategory.first
-    Subject.create(code: "ANNT", title: "Analytic Number Theory", subject_category: subject_category)
-    Subject.create(code: "ALNT", title: "Algebraic Number Theory", subject_category: subject_category)
+    Subject.create(code: "ANNT", title: "Analytic Number Theory", subject_category_id: subject_category)
+    Subject.create(code: "ALNT", title: "Algebraic Number Theory", subject_category_id: subject_category)
   end
 end
