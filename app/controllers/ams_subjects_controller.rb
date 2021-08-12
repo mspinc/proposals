841dd4061a7f7c957e7debbe3485a7669c443357
Banff International Research Station <brentk@birs.ca>
2021-08-12 11:49:05 -0600

    fix migration, acroynms

--- app/controllers/ams_subjects_controller.rb
+++ app/controllers/ams_subjects_controller.rb
@@ -7,19 +7,21 @@ class AmsSubjectsController < ApplicationController
   def update
     if @ams_subject.update(ams_subject_params)
       redirect_to subject_category_url(@subject_category),
-                  notice: 'Ams Subject was successfully updated'
+                  notice: 'AMS Subject was successfully updated'
     else
-      redirect_to edit_subject_category_ams_subject_path(@subject_category, @ams_subject),
+      redirect_to edit_subject_category_ams_subject_path(@subject_category,
+                                                         @ams_subject),
                   status: :unprocessable_entity,
-                  alert: "Unable to update ams subject."
+                  alert: "Unable to update AMS subject."
     end
   end
 
   def destroy
-    ams_subject = @ams_subject.ams_subject_categories.find_by(subject_category_id: @subject_category.id)
+    ams_subject = @ams_subject.ams_subject_categories
+                              .find_by(subject_category_id: @subject_category.id)
     ams_subject.destroy
     redirect_to subject_category_url(@subject_category),
-                notice: 'Ams Subject was successfully deleted.'
+                notice: 'AMS Subject was successfully deleted.'
   end
 
   private
--- db/migrate/20210717065007_update_subject_table.rb
+++ db/migrate/20210717065007_update_subject_table.rb
@@ -1,7 +1,11 @@
 class UpdateSubjectTable < ActiveRecord::Migration[6.1]
   def change
-    Subject.find_by(code: "ARNT")&.update(code: "ARGEO", title: "Arithmetic Geometry")
+    subject = Subject.find_by(code: "ARNT")
+    subject.update(code: "ARGEO", title: "Arithmetic Geometry") unless subject.nil?
+
     subject_category = SubjectCategory.first
+    return if subject_category.nil?
+
     Subject.create(code: "ANNT", title: "Analytic Number Theory", subject_category_id: subject_category.id)
     Subject.create(code: "ALNT", title: "Algebraic Number Theory", subject_category_id: subject_category.id)
   end
