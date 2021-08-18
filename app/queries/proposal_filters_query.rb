62467fd588197c239662fc74556aab50b5db0115
Hassan Mahmood <devhassan.mahmood@gmail.com>
2021-08-18 16:20:39 +0500

    Updated search filters

--- app/queries/proposal_filters_query.rb
+++ app/queries/proposal_filters_query.rb
@@ -7,51 +7,31 @@ class ProposalFiltersQuery
   end
   
   def find(params = {})
-    @result = filter_by_first_name(params[:firstname])
-    @result = filter_by_last_name(params[:lastname])
-    @result = filter_by_subject_area(params[:subject_area])
     @result = filter_by_keyword(params[:keywords])
     @result = filter_by_workshop_year(params[:workshop_year])
+    @result = filter_by_subject_area(params[:subject_area])
     @result = filter_by_proposal_type(params[:proposal_type])
     @result = filter_by_status(params[:status])
 
     @result
   end
-  
-  def filter_by_first_name(firstname)
-    return @result unless firstname.present?
-
-    @result.search_proposals(firstname)
-  end
-
-  def filter_by_last_name(lastname)
-    return @result unless lastname.present?
-
-    @result.search_proposals(lastname)
-  end
-
-  def filter_by_subject_area(subject_area)
-    return @result unless subject_area.present?
-
-    @result.search_proposal_subject(subject_area)
-  end
 
   def filter_by_keyword(keywords)
     return @result unless keywords.present?
 
-    if keywords == "active"
-      @result.search_proposals(1)
-    elsif keywords == "draft"
-      @result.search_proposals(0)
-    else
-      @result.search_proposals(keywords)
-    end
+    @result.search_proposals(keywords)
   end
 
   def filter_by_workshop_year(workshop_year)
     return @result unless workshop_year.present?
 
     @result.search_proposal_year(workshop_year)
+  end
+  
+  def filter_by_subject_area(subject_area)
+    return @result unless subject_area.present?
+
+    @result.search_proposal_subject(subject_area)
   end
 
   def filter_by_proposal_type(proposal_type)
