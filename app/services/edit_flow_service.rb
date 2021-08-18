50ece60ad30ef8c332e6b9e3e6360c25001cc325
Andrew Livingston <aj.livingston.dev@gmail.com>
2021-08-18 16:25:35 -0600

    Rearrange to visually match schema, add some notes

--- app/services/edit_flow_service.rb
+++ app/services/edit_flow_service.rb
@@ -18,33 +18,40 @@ class EditFlowService
     <<END_STRING
             mutation {
               article: submitArticle(data: {
+                sectionAbbrev: "#{proposal.subject.code}"
+
+                title: "#{proposal.title}"
+                abstract: "#{proposal.title}"
+
+                correspAuthorEmail: "#{proposal.lead_organizer.email}"
+
+                # just want to make clear we can do more than two authors -AJ
                 authors: [{
                   email: "#{proposal.lead_organizer.email}"
                   givenName: "#{proposal.lead_organizer.firstname}"
                   familyName: "#{proposal.lead_organizer.lastname}"
-                  nameInOriginalScript: "#{proposal.lead_organizer.fullname}"
+                  nameInOriginalScript: "#{proposal.lead_organizer.fullname}" # clarify: use nameInOriginalScript only for non-latin names, eg, "日暮 ひぐらし かごめ" -AJ
                   institution: "#{proposal.lead_organizer.affiliation}"
                   countryCode: "#{@country_code.alpha2}"
                 }, {
                   email: "#{@co_organizers.first.email}"
                   givenName: "#{@co_organizers.first.firstname}"
                   familyName: "#{@co_organizers.first.lastname}"
+                  mrAuthorID: 12345
                   institution: "#{@co_organizers.first.person.affiliation}"
                   countryCode: "#{@country_code_organizers.alpha2}"
-                  mrAuthorID: 12345
                 }]
-                correspAuthorEmail: "#{proposal.lead_organizer.email}"
-                title: "#{proposal.title}"
-                sectionAbbrev: "#{proposal.subject.code}"
+
                 primarySubjects: {
                   scheme: "MSC2020"
                   codes: ["00-01"]
                 }
+
                 secondarySubjects: {
                   scheme: "MSC2020"
                   codes: ["00B05", "00A07"]
                 }
-                abstract: "#{proposal.title}"
+
                 files: [{
                   role: "main"
                   key: "fileMain"
